e99e9dd9ae4856fe307ac311eedd78ec1e74becb
Александр Харченко <kharchenkoau31@gmail.com>
2020-02-13 12:15:57 +0200

    init commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by EVA RUSH on 2/13/20.
+//  Copyright © 2020 EVA RUSH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
